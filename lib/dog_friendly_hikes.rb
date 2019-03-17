5236c863755131e1560b5a95bfc88c52e50a4afc
Alyssa Brown <alyssa.brown@live.com>
2019-03-17 00:38:01 +0000

    first commit

---
+++ lib/dog_friendly_hikes.rb
@@ -0,0 +1,6 @@
+require "dog_friendly_hikes/version"
+
+module DogFriendlyHikes
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dog_friendly_hikes/version.rb
@@ -0,0 +1,3 @@
+module DogFriendlyHikes
+  VERSION = "0.1.0"
+end
