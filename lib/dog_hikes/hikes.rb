4cb2038febfd0513b746b4c4f7d09f0ea450d8b0
Alyssa Brown <alyssa.brown@live.com>
2019-03-17 03:19:40 +0000

    update

--- lib/dog_hikes/hikes.rb
+++ lib/dog_hikes/hikes.rb
@@ -1,18 +1,23 @@
-require 'pry'
+
 class DogHikes 
   def self.scrape_info
     hikes = []
-    hikes << self.hike_one 
-    hikes << self.hike_two 
+    hikes << self.scrape_hike_one  
+    hikes << self.scrape_hike_two  
     
   end 
   
-  def self.scrape_hike
+  def self.scrape_hike_one
    doc = Nokogiri::HTML (open("https://dayhikesneardenver.com/bear-canyon-loop-hike-ncar/"))
+   name = doc.search("h1.post entry-title").text 
+   location = doc.css("td.snapshotdetail").text 
+   distance = doc.css("td.snapshotdetail").text 
     binding.pry 
   end 
   
 end 
+
+
 
 
 
